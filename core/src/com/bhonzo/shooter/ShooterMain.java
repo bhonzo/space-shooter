6e2cc8ead2e69fcc045e46135752af0869da34de
bhonzo <kudziebone@gmail.com>
2015-03-02 11:52:35 +0000

    Integrated Map
    press up down left right to move the map

--- core/src/com/bhonzo/shooter/ShooterMain.java
+++ core/src/com/bhonzo/shooter/ShooterMain.java
@@ -2,26 +2,108 @@ package com.bhonzo.shooter;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
-public class ShooterMain extends ApplicationAdapter {
+public class ShooterMain extends ApplicationAdapter implements InputProcessor {
 	SpriteBatch batch;
-	Texture img;
+	//Texture img;
+	TiledMap tiledMap;
+    OrthographicCamera camera;
+    TiledMapRenderer tiledMapRenderer;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		//img = new Texture("badlogic.jpg");
+	//
+		 float w = Gdx.graphics.getWidth();
+	        float h = Gdx.graphics.getHeight();
+
+	        camera = new OrthographicCamera();
+	        camera.setToOrtho(false,w,h);
+	        camera.update();
+	        tiledMap = new TmxMapLoader().load("level1map.tmx");
+	        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+		  
+		
+		//to listen to user inoput 
+		Gdx.input.setInputProcessor(this);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		 camera.update();
+	        tiledMapRenderer.setView(camera);
+	        tiledMapRenderer.render();
 		batch.begin();
-		batch.draw(img, 0, 0);
+	//	batch.draw(img, 0, 0);
 		batch.end();
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		 if(keycode == Input.Keys.LEFT)
+	            camera.translate(-32,0);
+	        if(keycode == Input.Keys.RIGHT)
+	            camera.translate(32,0);
+	        if(keycode == Input.Keys.UP)
+	            camera.translate(0,-32);
+	        if(keycode == Input.Keys.DOWN)
+	            camera.translate(0,32);
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
